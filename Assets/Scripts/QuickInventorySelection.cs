95e3d8731d155bc12672edb3cc3254aa78fa1c6d
FireboltLegend <abbas.khawaja@gmail.com>
2024-12-26 12:36:28 -0600

    Added Sprites to Folder

--- Assets/Scripts/QuickInventorySelection.cs
+++ Assets/Scripts/QuickInventorySelection.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using Microsoft.Unity.VisualStudio.Editor;
+using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuickInventorySelection : MonoBehaviour
 {
 	[SerializeField] private GameObject[] inventoryImages;
+	[SerializeField] private Sprite bar;
+	[SerializeField] private Sprite selBar;
+
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -17,7 +22,36 @@ public class QuickInventorySelection : MonoBehaviour
 	{
 		if(Input.GetKeyDown(KeyCode.Alpha1))
 		{
-
+			removeSprites();
+			inventoryImages[0].GetComponent<UnityEngine.UI.Image>().sprite = selBar;
+		}
+		else if(Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			removeSprites();
+			inventoryImages[1].GetComponent<UnityEngine.UI.Image>().sprite = selBar;
+		}
+		else if(Input.GetKeyDown(KeyCode.Alpha3))
+		{
+			removeSprites();
+			inventoryImages[2].GetComponent<UnityEngine.UI.Image>().sprite = selBar;
+		}
+		else if(Input.GetKeyDown(KeyCode.Alpha4))
+		{
+			removeSprites();
+			inventoryImages[3].GetComponent<UnityEngine.UI.Image>().sprite = selBar;
+		}
+		else if(Input.GetKeyDown(KeyCode.Alpha5))
+		{
+			removeSprites();
+			inventoryImages[4].GetComponent<UnityEngine.UI.Image>().sprite = selBar;
+		}
+	}
+	
+	private void removeSprites()
+	{
+		foreach(GameObject image in inventoryImages)
+		{
+			image.GetComponent<UnityEngine.UI.Image>().sprite = bar;
 		}
 	}
 }
