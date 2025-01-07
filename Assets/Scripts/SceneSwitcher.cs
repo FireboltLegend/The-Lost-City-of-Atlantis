8b27ec4eccfe1a839a90972bcb44a9c1bc1dd28e
Hrishikesh S. <hrishikeshs2005@gmail.com>
2025-01-07 12:28:41 -0600

    Changed SceneLoader script to SceneSwitcher

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour
+public class SceneSwitcher : MonoBehaviour
 {
     [SerializeField] private string sceneName;
 
