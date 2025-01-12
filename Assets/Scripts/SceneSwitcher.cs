a2a8820f8512744efe721748a753350f6acc6252
FireboltLegend <abbas.khawaja@gmail.com>
2025-01-12 16:20:59 -0600

    Fixing Starting Position

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -6,9 +6,27 @@ using UnityEngine.SceneManagement;
 public class SceneSwitcher : MonoBehaviour
 {
     [SerializeField] private string sceneName;
+    [SerializeField] private Vector3 startPosition;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         SceneManager.LoadScene(sceneName);
+        SceneManager.sceneLoaded += (scene, mode) =>
+		{
+			PlayerController player = FindObjectOfType<PlayerController>();
+			if(player != null)
+			{
+				player.level = 2;
+				// Debug.Log(data.position[0] + " " + data.position[1] + " " + data.position[2]);
+				player.transform.position = startPosition;
+				player.health = 100;
+				player.oxygen = 100;
+			}
+			else
+			{
+				Debug.LogError("Level Couldn't be Loaded");
+			}
+			SceneManager.sceneLoaded -= (scene, mode) => { }; 
+		};
     }
 }
