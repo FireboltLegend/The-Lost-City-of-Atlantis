51eac15347a9a464be26fa3a1ed9babf49c85c7b
Hrishikesh S. <hrishikeshs2005@gmail.com>
2025-01-12 21:54:25 -0600

    Modified difficulties of levels and added scene switching between levels

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    [SerializeField] private Vector2 min;
+    [SerializeField] private Vector2 max;
+
     private Transform player;
+    private Camera cam;
 
     private void Start()
     {
         player = FindObjectOfType<PlayerController>().transform;
+        cam = Camera.main;
     }
 
     private void LateUpdate()
     {
         transform.position = player.position + Vector3.forward * -10;
+
+        Vector3 newPosition = Vector3.zero;
+        newPosition.x = Mathf.Clamp(transform.position.x, min.x, max.x);
+        newPosition.y = Mathf.Clamp(transform.position.y, min.y, max.y);
+        newPosition.z = -10;
+        transform.position = newPosition;
     }
 }
